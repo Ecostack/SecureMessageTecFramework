1238a51b6205bdb4a44cce3a6f6a20c7e0b38333
Sebastian <Sebastian@N4wuk-Laptop>
2013-10-02 22:41:40 +0200

    Exception hinzugefügt

---
+++ SecureMessageTecFramework/src/main/java/de/bio/hazard/securemessage/tecframework/exception/EncryptionExceptionBiohazard.java
@@ -0,0 +1,8 @@
+package de.bio.hazard.securemessage.tecframework.exception;
+
+public class EncryptionExceptionBiohazard extends Exception {
+
+	public EncryptionExceptionBiohazard() {
+		super("Problem with encryption / decryption occured.");
+	}
+}
