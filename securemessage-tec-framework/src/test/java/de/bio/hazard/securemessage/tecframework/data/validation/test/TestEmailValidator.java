5ef063767d9fd742c1923b7eeed1ca109e843dad
Sebastian <Sebastian@192.168.0.100>
2013-10-23 20:57:23 +0200

    Auf Annotation umgestellt

--- securemessage-tec-framework/src/test/java/de/bio/hazard/securemessage/tecframework/data/validation/test/TestEmailValidator.java
+++ securemessage-tec-framework/src/test/java/de/bio/hazard/securemessage/tecframework/data/validation/test/TestEmailValidator.java
@@ -1,32 +1,40 @@
 package de.bio.hazard.securemessage.tecframework.data.validation.test;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import junit.framework.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import de.bio.hazard.securemessage.tecframework.data.validation.EmailValidator;
 
-public class TestEmailValidator extends TestCase {
+public class TestEmailValidator {
 
-	private EmailValidator emailValidator;
+	private static EmailValidator emailValidator;
 
-	public void setUp() {
+	@BeforeClass
+	public static void setUp() {
 		emailValidator = new EmailValidator();
 	}
 
-	public String[] validMails = { "testme@yahoo.com", "testme-100@yahoo.com",
-			"testme.100@yahoo.com", "testme111@testme.com",
-			"testme-100@testme.net", "testme.100@testme.com.au",
-			"testme@1.com", "testme@gmail.com.com", "testme+100@gmail.com",
-			"testme-100@yahoo-test.com" };
+	public static String[] validMails = { "testme@yahoo.com",
+			"testme-100@yahoo.com", "testme.100@yahoo.com",
+			"testme111@testme.com", "testme-100@testme.net",
+			"testme.100@testme.com.au", "testme@1.com", "testme@gmail.com.com",
+			"testme+100@gmail.com", "testme-100@yahoo-test.com" };
 
-	public String[] invalidMails = { "testme", "testme@.com.my",
+	public static String[] invalidMails = { "testme", "testme@.com.my",
 			"testme123@gmail.a", "testme123@.com", "testme123@.com.com",
 			".testme@testme.com", "testme()*@gmail.com", "testme@%*.com",
 			"testme..2002@gmail.com", "testme.@gmail.com",
 			"testme@testme@gmail.com", "testme@gmail.com.1a" };
 
+	@Test
 	public void testValidMailsByFormat() {
 		checkMailsByFormat(validMails, true);
 	}
 
+	@Test
 	public void testInvalidMailsByFormat() {
 		checkMailsByFormat(invalidMails, false);
 	}
